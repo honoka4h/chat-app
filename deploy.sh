69c638bbfda23cc9680ff0fae33a4250a1dcd8c0
honoka4h <honoka4h@gmail.com>
2025-12-06 16:26:12 +0900

    test

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Backend
+cd /var/www/chat-app/Backend
+git reset --hard
+git pull origin main
+npm install
+npm run build
+pm2 restart chat-app || pm2 start /dist/main.js  --name "chat-app"
+
+# Frontend
+cd /var/www/chat-app/Frontend
+git reset --hard
+git pull origin main
+npm install
+npm run build
+pm2 restart index || pm2 start /.output/server/index.mjs --name "index"
