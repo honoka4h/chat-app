c2cf6d19d204fe20d9c48ec4cad6cff7eedad5cc
honoka4h <honoka4h@gmail.com>
2025-12-14 20:37:53 +0900

    chore: enable HTTPS and apply custom domain

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,4 @@ cd /var/www/chat-app/Frontend
 git pull origin main
 npm install
 npm run build
-pm2 restart frontend || pm2 start ecosystem.config.cjs --only frontend
-
-
+pm2 restart index || pm2 start .output/server/index.mjs --name "index"
